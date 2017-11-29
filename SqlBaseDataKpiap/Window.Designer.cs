6550ed5648eb8633f212c9667013c7666cac294c
Alex <sashafriend2000@mail.ru>
2017-11-29 18:39:57 +0300

    Добавьте файлы проекта.

---
+++ SqlBaseDataKpiap/Window.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SqlBaseDataKpiap
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.Data_Table = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BookName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateOfPublish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Data_Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Data_Table
+            // 
+            this.Data_Table.AllowUserToAddRows = false;
+            this.Data_Table.AllowUserToResizeColumns = false;
+            this.Data_Table.AllowUserToResizeRows = false;
+            this.Data_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Data_Table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BookName,
+            this.DateOfPublish,
+            this.Genre,
+            this.Mark});
+            this.Data_Table.Location = new System.Drawing.Point(12, 12);
+            this.Data_Table.MultiSelect = false;
+            this.Data_Table.Name = "Data_Table";
+            this.Data_Table.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Data_Table.Size = new System.Drawing.Size(606, 306);
+            this.Data_Table.TabIndex = 0;
+            this.Data_Table.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.Data_Table_CellEndEdit);
+            this.Data_Table.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.Data_Table_UserDeletingRow);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(720, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(720, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(720, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(720, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(720, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ADDDDDDD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BookName
+            // 
+            this.BookName.HeaderText = "Название книги";
+            this.BookName.Name = "BookName";
+            this.BookName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.BookName.Width = 200;
+            // 
+            // DateOfPublish
+            // 
+            this.DateOfPublish.HeaderText = "Год публикации";
+            this.DateOfPublish.Name = "DateOfPublish";
+            this.DateOfPublish.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.DateOfPublish.Width = 110;
+            // 
+            // Genre
+            // 
+            this.Genre.HeaderText = "Жанр";
+            this.Genre.Name = "Genre";
+            this.Genre.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Genre.Width = 150;
+            // 
+            // Mark
+            // 
+            this.Mark.HeaderText = "Оценка";
+            this.Mark.Name = "Mark";
+            this.Mark.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 334);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Data_Table);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Window";
+            this.Text = "Окошечко";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Window_FormClosing);
+            this.Load += new System.EventHandler(this.Window_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Data_Table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Data_Table;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateOfPublish;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mark;
+    }
+}
+
---
+++ SqlBaseDataKpiap/Window.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlBaseDataKpiap
+{
+    public partial class Window : Form
+    {
+        SqlConnection connection;
+        SqlDataReader ReaderSql;
+        SqlCommand command;
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\Visual Studio 2017\Projects\SqlBaseDataKpiap\SqlBaseDataKpiap\DataBase.mdf;Integrated Security=True");
+            // типа присоеденились к базе данных через её строку подключения
+
+            connection.Open();
+            command = new SqlCommand("SELECT * FROM [Books]", connection);
+            try
+            {
+                ReaderSql = command.ExecuteReader();
+                while (ReaderSql.Read())
+                    Data_Table.Rows.Add(ReaderSql["BookName"], ReaderSql["YearPubl"], ReaderSql["Genre"], ReaderSql["Mark"]);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("А че-то не так с БД, ща скажем почему... о, во причина: " + exp.Source.ToString());
+            }
+            if (ReaderSql != null)
+                ReaderSql.Close();
+
+        }
+
+        private void Window_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connection != null && connection.State != ConnectionState.Closed)
+                connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Все поля должны быть заполнены", "Ошебка");
+                return;
+            }
+            command = new SqlCommand("INSERT INTO [Books] (BookName, YearPubl, Genre, Mark)VALUES(@BookName, @YearPubl, @Genre, @Mark)", connection);
+
+            command.Parameters.AddWithValue("BookName", textBox1.Text);
+            command.Parameters.AddWithValue("YearPubl", textBox2.Text);
+            command.Parameters.AddWithValue("Genre", textBox3.Text);
+            command.Parameters.AddWithValue("Mark", textBox4.Text);
+            command.ExecuteNonQuery();
+            Data_Table.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+        }
+
+        private void Data_Table_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            command = new SqlCommand("DELETE FROM [Books] WHERE [ID] = @ID", connection);
+            command.Parameters.AddWithValue("ID", e.Row.Index + 1);
+            command.ExecuteNonQuery();
+        }
+
+        private void Data_Table_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            string polySettings = default;
+            string poly = default;
+            switch (e.ColumnIndex)
+            {
+                case 0:
+                    polySettings = "[BookName] = @BookName";
+                    poly = "BookName";
+                    break;
+                case 1:
+                    polySettings = "[YearPubl] = @YearPubl";
+                    poly = "YearPubl";
+                    break;
+                case 2:
+                    polySettings = "[Genre] = @Genre";
+                    poly = "Genre";
+                    break;
+                case 3:
+                    polySettings = "[Mark] = @Mark";
+                    poly = "Mark";
+                    break;
+            }
+            command = new SqlCommand("UPDATE [Books] SET " + polySettings + " WHERE [ID] = @ID", connection);
+            command.Parameters.AddWithValue(poly, Data_Table.CurrentCell.Value.ToString());
+            command.Parameters.AddWithValue("ID", Data_Table.CurrentRow.Index + 1);
+            command.ExecuteNonQuery();
+        }
+    }
+}
